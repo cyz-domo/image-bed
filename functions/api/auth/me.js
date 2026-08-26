7bd65ec31431ec459e28c62236043769be8116c6
cyz <92909805+cyz-domo@users.noreply.github.com>
2026-08-26 13:19:29 +0800

    chore: remove obsolete functions path

--- functions/api/auth/me.js
+++
@@ -1,3 +0,0 @@
-import { readSession } from "../../_lib/auth.js";
-import { json } from "../../_lib/http.js";
-export async function onRequest({ request, env }) { const session = await readSession(request, env); return json(session ? { authenticated: true, login: session.login } : { authenticated: false }); }
