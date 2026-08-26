a1830ce5d74bda24e24cc8f5daf754313f0ea285
cyz <92909805+cyz-domo@users.noreply.github.com>
2026-08-26 13:19:27 +0800

    chore: remove obsolete functions path

--- functions/api/auth/logout.js
+++
@@ -1,4 +0,0 @@
-import { clearSessionCookie } from "../../_lib/auth.js";
-import { json } from "../../_lib/http.js";
-export async function onRequest() { return json({ ok: true }, 200, { "Set-Cookie": clearSessionCookie() }); }
-
